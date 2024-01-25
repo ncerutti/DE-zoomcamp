cd8d6090578d198b9235abc919e5d9af3694bcee
nico <nc@nicores.de>
2024-01-25 00:25:07 +0000

    add gcp bucket to terraform

--- 2_terraform/main.tf
+++ 2_terraform/main.tf
@@ -1,14 +1,30 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "5.13.0"
     }
   }
 }
 
 provider "google" {
-    credentials = file(".keys/dezoom-terrasecret.json")
-  project = "dezoom-412200"
-  region = "europe-west2"
+  credentials = file("./keys/dezoom-terrasecret.json")
+  project     = "dezoom-412200"
+  region      = "europe-west2"
+}
+
+resource "google_storage_bucket" "dezoom_bucket" {
+  name          = "bucket-dezoom-412200"
+  location      = "EU"
+  storage_class = "STANDARD"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
 }
